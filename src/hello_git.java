0a5019d14c0aaafb622416729ac4788a1d0b413b
hexuehui <hexuehui@corp.netease.com>
2020-06-24 10:41:27 +0800

    test

--- src/hello_git.java
+++ src/hello_git.java
@@ -2,5 +2,9 @@ public class hello_git {
     public static void main(String[] args) {
         System.out.println("hello git!!!!");
         System.out.println("hello git!!!!");
+        System.out.println("hello git!!!!");
+        System.out.println("hello git!!!!");
+        System.out.println("hello git!!!!");
+        System.out.println("hello git!!!!");
     }
 }
